2f609552e69846715cc5b0c13e6681d57330358a
Prince Thakur <pt48344@gmail.com>
2026-05-13 21:02:53 +0530

    restore the builder template

--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,15 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-// pages 
-
-
-// layouts
-
-import { AuthLayout, DashboardLayout } from '../layout/index'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Builder,
+  Dashboard,
+  Home,
+  Login,
+  NotFound,
+  Register,
+  Template,
+} from "../pages/index";
+import { AuthLayout, DashboardLayout } from "../layout/index";
 
 
 const AppRoutes = () => {
@@ -14,20 +17,29 @@ const AppRoutes = () => {
     <div>
       <Router>
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/user/login' element={<Login/>} />
-            <Route path='/user/register' element={<Register/>} />
-            <Route path='/user/notfound' element={<NotFound/>} />
-            <Route path='/user/templates' element={<Templates/>} />
-            <Route path='/dashboard' element={<Dashboard/>} />
-            <Route path= '/authlayout' element={<AuthLayout/>} />
-            <Route path= '/dashboardlayout' element={<DashboardLayout/>} />
-
-            
+          <Route path="/" element={<Home />} />
+          <Route path="/user/login" element={<Login />} />
+          <Route path="/user/register" element={<Register />} />
+          <Route path="/user/notfound" element={<NotFound />} />
+          <Route path="/user/templates" element={<Template />} />
+          <Route path="/builder/:templateId" element={<Builder />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/authlayout"
+            element={<AuthLayout>Auth Layout Preview</AuthLayout>}
+          />
+          <Route
+            path="/dashboardlayout"
+            element={
+              <DashboardLayout>
+                Dashboard Layout Preview
+              </DashboardLayout>
+            }
+          />
         </Routes>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
